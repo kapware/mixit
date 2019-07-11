484ed88841061b91e1d8235e3180088a800235ba
Pawel Kapala <github@kapware.com>
2019-07-11 12:00:09 +0200

    Project generated from start.spring.io

---
+++ src/main/java/com/example/mixit/MixitApplication.java
@@ -0,0 +1,13 @@
+package com.example.mixit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixitApplication.class, args);
+	}
+
+}
